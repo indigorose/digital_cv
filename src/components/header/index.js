024a542dd0cbd38d944602c50d0f7d77ff55e60b
indigorose <indigorose049@gmail.com>
2022-09-21 05:43:24 +0100

    Add new components and modify header styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,29 +1,62 @@
+import React from "react";
 import "./index.scss";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faContactCard,
+  faHome,
+  faLayerGroup,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Headerbar = () => {
-  <div className="nav-bar">
-    <h1>hello</h1>
-    {/* Think about a home log here */}
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-  </div>;
+  return (
+    <>
+      <div className="nav-bar">
+        {/* Think about a home log here */}
+        <nav className="header-wrapper">
+          <div className="namepage">
+            <NavLink to="/" className="brand">
+              Karen Robertson's Website
+            </NavLink>
+          </div>
+          {/* TODO - think about the projects link being a dropdown? */}
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            Home
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            About
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="project-link"
+            to="/"
+          >
+            <FontAwesomeIcon icon={faLayerGroup} color="#4d4d4e" />
+            Projects
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/"
+          >
+            <FontAwesomeIcon icon={faContactCard} color="#4d4d4e" />
+            Contact
+          </NavLink>
+        </nav>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Headerbar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./index.scss";
 import Headerbar from "./header";
 
