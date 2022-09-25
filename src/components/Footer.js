528a80d5166af1c32bf6aaf2cc14ed23271396e6
indigorose <indigorose049@gmail.com>
2022-09-25 18:31:33 +0100

    Update Navbar and footer functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTwitter,
+  faLinkedin,
+  faCodepen,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -10,14 +15,29 @@ const Footer = () => {
       <section className="footer">
         <div className="footer-container">
           <div className="left">
-            <div className="location">
-              <FontAwesomeIcon icon={faHome} color="#000" />
-            </div>
-            <p>London</p>
             <div className="mail">
               <h4>
-                <FontAwesomeIcon icon={faEnvelope} color="#000" />
-                Email
+                <FontAwesomeIcon icon={faEnvelope} color="#fff" /> Email
+              </h4>
+            </div>
+            <div className="github">
+              <h4>
+                <FontAwesomeIcon icon={faGithub} color="#fff" /> GitHub
+              </h4>
+            </div>
+            <div className="codepen">
+              <h4>
+                <FontAwesomeIcon icon={faCodepen} color="#fff" /> Codepen
+              </h4>
+            </div>
+            <div className="twitter">
+              <h4>
+                <FontAwesomeIcon icon={faTwitter} color="#fff" /> Twitter
+              </h4>
+            </div>
+            <div className="linkedin">
+              <h4>
+                <FontAwesomeIcon icon={faLinkedin} color="#fff" /> Linkedin
               </h4>
             </div>
           </div>
@@ -27,10 +47,6 @@ const Footer = () => {
               A web developer from London, UK. I enjoy building new projects
               using new technologies and all things pop culture.
             </p>
-          </div>
-          <div className="social">
-            <FontAwesomeIcon icon={faTwitter} color="#000" size="xl" />
-            <FontAwesomeIcon icon={faLinkedin} color="#000" size="xl" />
           </div>
         </div>
       </section>
